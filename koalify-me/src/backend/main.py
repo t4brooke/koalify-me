3d10c043c515fb2d817d6dfa4c64de77a0d266ed
Andersen Hu <andersenhu23@gmail.com>
2025-03-01 16:52:44 -0500

    Added file for resume analysis compared to job description and options on main to ask about the resume analysis

--- koalify-me/src/backend/main.py
+++ koalify-me/src/backend/main.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import os
 
 import quizAndAnswers
+import resumeRec
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", ""))
 
@@ -25,3 +26,14 @@ if __name__ == "__main__":
 
     result = client.chat.completions.create(**params)
     print(result.choices[0].message.content)
+
+    print("Would you like to have your resume analyzed to a job/internship description you want to apply to?\n")
+    answerToResAnlys = input("Please enter 'y' or 'n'\n")
+    while answerToResAnlys.lower()!=("y" or "n"):
+        print("Please enter a valid response.\n")
+        answerToResAnlys = input("Please enter 'y' or 'n'")
+    
+    if answerToResAnlys.lower() == "n":
+        print("")
+    elif answerToResAnlys.lower() == "y":
+        resumeRec.resumeAnalysis() 
