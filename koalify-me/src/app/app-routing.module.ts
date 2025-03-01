9a15f2d96c75f78ad5867ad45063c791fb7dacac
zl537 <zl537@drexl.edu>
2025-03-01 16:52:50 -0500

    basic sign-up page

--- koalify-me/src/app/app-routing.module.ts
+++ koalify-me/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomePageComponent }
+  { path: '', component: WelcomePageComponent },
+  { path: 'signup', component: SignupPageComponent }
 ];
 
 @NgModule({
---
+++ koalify-me/src/app/signup-page/signup-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  standalone: false,
+  templateUrl: './signup-page.component.html',
+  styleUrl: './signup-page.component.css'
+})
+export class SignupPageComponent {
+
+}
