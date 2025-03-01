cd6d6288d074e49d616ab8c74b993615e34acead
Andersen Hu <andersenhu23@gmail.com>
2025-03-01 14:29:37 -0500

    Added main for AI functions and quiz file

---
+++ koalify-me/src/backend/quizAndAnswers.py
@@ -0,0 +1,11 @@
+def quiz():
+    print("Enter your answers to the questions")
+    quizQuestion = ["aaa",
+                    "bbb",
+                    "ccc",
+                    ]
+    answer = []
+    for question in quizQuestion:
+        questionCount = 1
+        print(question)
+        quizAnswers = answer.append(input(""))
