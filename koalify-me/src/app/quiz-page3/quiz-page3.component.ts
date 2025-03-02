604dd3c587e7264ab2907aab92a0763eb99f218d
brookebrooks <bib26@drexel.edu>
2025-03-02 11:54:11 -0500

    randomizing majors for blank entry

--- koalify-me/src/app/quiz-page3/quiz-page3.component.ts
+++ koalify-me/src/app/quiz-page3/quiz-page3.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-page3',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './quiz-page3.component.css'
 })
 export class QuizPage3Component {
+  constructor(private router: Router) { }
 
+  onSubmit(): void {
+    const resultPages = [
+      '/results-game-dev',
+      '/results-compsci',
+      '/results-ui-ux',
+      '/results-cyber',
+      '/results-softeng',
+      '/results-info-sys',
+      '/results-info-tech',
+      '/results-ai-ml',
+      '/results-comp-eng',
+      '/results-data-sci'
+    ];
+
+    const randomPage = resultPages[Math.floor(Math.random() * resultPages.length)];
+
+    this.router.navigate([randomPage]);
+  }
 }
